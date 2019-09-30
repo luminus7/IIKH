06c423ebd27dd65557943e06fe1e14c788872870
robinjoo1015 <38099263+robinjoo1015@users.noreply.github.com>
2019-09-30 18:54:46 +0900

    Add Date header file

---
+++ Date.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Meal.h"
+
+class Date {
+private:
+	int year, month, day;
+	Meal breakfast, lunch, dinner;
+
+public:
+	bool init(int year, int month, int day);
+	void setBreakfast(Meal breakfast) { this->breakfast = breakfast; }
+	void setLunch(Meal lunch) { this->lunch = lunch; }
+	void setDinner(Meal dinner) { this->dinner = dinner; }
+	int getYear() { return year; }
+	int getMonth() { return month; }
+	int getDay() { return day; }
+	Meal getBreakfast() { return breakfast; }
+	Meal getLunch() { return lunch; }
+	Meal getDinner() { return dinner; }
+};
